ad4d10ac461295ee8f10d23985aed3064d2b74c2
peterennis <peterennis@yahoo.com>
2015-07-17 10:41:24 -0700

    extract bemb.v0.5.0.0.zip

---
+++ bemb/demoChartJS/bembCustom.js
@@ -0,0 +1,21 @@
+/* this is used by the bemb project */
+
+var Canvas;
+
+function refreshChart(chartData, options) {
+
+	Canvas = document.getElementById("canvas");
+	var ctx = Canvas.getContext("2d");
+		
+	window.myLine = new Chart(ctx).Line(chartData, options);
+	
+}
+
+
+
+
+
+
+
+
+
---
+++ bemb/snippets.js
@@ -0,0 +1,36 @@
+/// StartSnippet: WindowResizedHandlerSnippet
+Canvas.width = window.innerWidth;
+Canvas.height = window.innerHeight;
+
+/// StartSnippet: ChartClickHandlerSnippet
+var points = window.myLine.getPointsAtEvent(event);
+bembData = points[0].value + ', ' + points[1].value;
+
+/// StartSnippet: RefreshChartDataBuild
+refreshChart({
+	labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+	datasets: [{
+		label: 'Dataset 1 Name',
+		fillColor: 'rgba(220,220,220,0.2)',
+		strokeColor: 'rgba(220,220,220,1)',
+		pointStrokeColor: '#fff',
+		pointHighlightFill: '#fff',
+		pointHighlightStroke: 'rgba(220,220,220,1)',
+		data: [|DATASET1|]
+	}, {
+		label: 'Dataset 2 Name',
+		fillColor: 'rgba(151,187,205,0.2)',
+		strokeColor: 'rgba(151,187,205,1)',
+		pointStrokeColor: '#fff',
+		pointHighlightFill: '#fff',
+		pointHighlightStroke: 'rgba(151,187,205,1)',
+		data: [|DATASET2|]
+	}]
+}, {
+	responsive: true,
+	bezierCurve: true,
+	pointHitDetectionRadius: 5,
+	pointDotRadius: 5,
+	datasetStrokeWidth: 1,
+	bezierCurveTension: 0.4
+});
